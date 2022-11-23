4ea6b19fdf9984f4f808c4600ae988899720b4fa
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2022-11-22 20:19:00 -0800

    basic functionality finished

--- src/lex.hs
+++ src/lex.hs
@@ -2,31 +2,64 @@ import System.IO
 import System.Environment
 import Control.Monad
 
-data BraceMod = Default | Module
+data BraceMod = Default | Module | ModuleOpen
+
+innerUnmacro :: String -> String
+innerUnmacro [] = []
+innerUnmacro (':':[]) = ":0]"
+innerUnmacro (x:xs) = x:(innerUnmacro xs)
 
 unmacro :: [BraceMod] -> String -> (String,[BraceMod]->[BraceMod])
-unmacro _ "mod" = ("module",\s->[Module]++s)
+unmacro _ "mod" = ("module",\s->[ModuleOpen, Module]++s)
 unmacro _ "i" = ("input",id)
+unmacro _ "o" = ("output",id)
 unmacro _ "l" = ("logic",id)
 unmacro _ "ff" = ("always_ff @",id)
-unmacro (Module:_) "{" = (";",id)
-unmacro _ "{" = ("begin ",id)
+unmacro (ModuleOpen:_) "{" = (";",tail)
+unmacro _ "{" = ("begin",\s->[Default]++s)
 unmacro (Module:_) "}" = ("endmodule",tail)
-unmacro _ "}" = ("end",id)
-unmacro _ x = (x,id)
+unmacro _ "}" = ("end",tail)
+unmacro _ "->" = ("assign",id)
+unmacro _ x = (innerUnmacro x,id)
 
 translate :: [BraceMod] -> [String] -> [String]
 translate s [] = []
 translate s (x:xs) = [(fst$unmacro s x)]++(translate ((snd$unmacro s x) s) xs)
 
 splitParens :: [String] -> [String]
-splitParens (('(':xs):ks) = ["("]++[xs]++ks
-splitParens (('{':xs):ks) = ["{"]++[xs]++ks
-splitParens (x:xs) = [x]++xs
+splitParens (('(':xs):ks) = ["("]++[xs]++(splitParens ks)
+splitParens (('{':xs):ks) = ["{"]++[xs]++(splitParens ks)
+splitParens (x:xs) = [x]++(splitParens xs)
 splitParens [] = []
+--splitParens (('[':xs):ks) = ["["]++[xs]++(splitParens ks)
 
 process :: String -> String
 process code = unwords $ translate [] $ splitParens $ words code
+
+--  initial tabbing -> string to prettify -> output
+prettify :: String -> String -> String
+prettify t [] = []
+prettify t (';':xs) = ";\n"++t++(prettify t xs)
+prettify t ('b':'e':'g':'i':'n':xs) = "begin\n"++"\t"++t++(prettify ("\t"++t) xs)
+prettify t ('e':'n':'d':'m':'o':'d':'u':'l':'e':xs) = "endmodule\n"++(prettify (drop 1 t) xs)
+prettify t ('e':'n':'d':xs) = "end\n"++(drop 1 t)++(prettify (drop 1 t) xs)
+prettify t ('m':'o':'d':'u':'l':'e':xs) = "module"++(prettify ("\t"++t) xs)
+prettify t (x:xs) = x:(prettify t xs)
+
+unspace :: String -> String
+unspace [] = []
+unspace (x:' ':'(':xs) = x:'(':(unspace xs)
+unspace ('(':' ':xs) = '(':(unspace xs)
+unspace (' ':' ':xs) = ' ':(unspace xs)
+unspace (' ':';':xs) = ';':(unspace xs)
+unspace (';':' ':xs) = ';':(unspace xs)
+unspace (x:xs) = x:(unspace xs)
+
+untab :: String -> String
+untab [] = []
+untab ('\t':'e':'n':'d':xs) = "end"++(untab xs)
+untab ('\t':' ':'e':'n':'d':xs) = "end"++(untab xs)
+untab (x:xs) = x:(untab xs)
 
 main :: IO ()
 main = do
@@ -35,6 +68,6 @@ main = do
         do {
             handle <- openFile (head filename) ReadMode
         ;   prg <- hGetContents handle
-        ;   print $ process prg
+        ;   putStrLn$ untab $ prettify "" $ unspace . unspace $ process prg
         ;   hClose handle
         }
